ef52f75d4b9a91d7e812e1a50601ff5526c05a06
Tiana0117 <yining0117@g.ucla.edu>
2021-12-01 01:44:31 -0800

    app updating

--- app/app.py
+++ app/app.py
@@ -7,15 +7,20 @@ print(sys.path)
 
 import pandas as pd
 import json
+import spotipy
+import spotipy.oauth2
+import requests
 
+import numpy as np
+
+import re
+import string
 
 app = Flask(__name__)
 
-# import tensorflow as tf 
-# from tensorflow import keras
+import tensorflow as tf
 import numpy as np 
-
-# from tensorflow.keras.models import load_model 
+from tensorflow import keras 
 # from tensorflow.keras.backend import set_session
 # from skimage.transform import resize 
 
@@ -34,6 +39,60 @@ def main():
 
 # def hello():
     # return render_template("hello.html")
+def get_track_features(track_id):
+    """
+    Access the input of a song's Spotify ID,
+    output the features in a dataframe
+    """
+    # use Client ID and Client Secret for Spotify Developer
+    CLIENT_ID = "8e256a7b32f845ec9aebf81ae0feadb4"
+    CLIENT_SECRET = '928d1a35600041eea6c69f97205fac6c'
+    
+    AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+    # POST
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    })
+
+    # convert the response to JSON
+    auth_response_data = auth_response.json()
+
+    # save the access token
+    access_token = auth_response_data['access_token']
+    
+    headers = {
+        'Authorization': 'Bearer {token}'.format(token=access_token)
+    }
+    
+    # base URL of all Spotify API endpoints
+    BASE_URL = 'https://api.spotify.com/v1/'
+
+    # Track ID from the URI
+    track_id = track_id
+
+    # actual GET request with proper header
+    r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+    r = r.json()
+    
+    # r is a dictionary with our desired feature and values
+    # transform the dictionary into a dataframe
+    # drop the unnecessary columns
+    features = pd.DataFrame(r,index = [0])
+    features = features.drop(["uri","track_href","analysis_url","type","id","key","mode","speechiness","tempo","duration_ms","time_signature","liveness"],axis = 1)
+    print(features.columns)
+    
+    return features.astype(np.float32)
+
+@tf.keras.utils.register_keras_serializable()
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation),
+                                  '')
 
 @app.route("/submit/", methods = ["POST", "GET"])
 def submit():
@@ -41,90 +100,30 @@ def submit():
         return render_template("submit.html")
     else:
         try:
+            output = get_track_features(request.form['song'])
+            lyrics = request.form["lyrics"]           
+
+            output = get_track_features(request.form['song'])
             lyrics = request.form["lyrics"]
+            dictionary = {'lyrics':[lyrics]}
+            lyrics = pd.DataFrame(dictionary,index=[0])
 
-            # size_vocabulary = 2000
+            lyrics_input = keras.Input(
+                shape = (1,), 
+                name = "lyrics",
+                dtype = "string")
 
-            # vectorize_layer = TextVectorization(
-            #     standardize=standardization,
-            #     max_tokens= size_vocabulary, # only consider this many words
-            #     output_mode='int',
-            #     output_sequence_length=500) 
+            scalars_input = keras.Input(
+                shape = (len(output),), 
+                name = "output",
+                dtype = "float64")
 
-            # vectorize_layer.adapt(train.map(lambda x, y: x["lyrics"]))
+            model = tf.keras.models.load_model("/Users/yiningliang/Desktop/PIC16B-Gitthub/music-classifier/app/mnist-model/fake_model4")
+            prediction = model.predict([lyrics_input,scalars_input])
 
-            # scalars_input = keras.Input(
-            #     shape = (len(scalars),), 
-            #     name = "scalars",
-            #     dtype = "float64"
-            # )
-            # lyrics_input = keras.Input(
-            #     shape = (1,), 
-            #     name = "lyrics",
-            #     dtype = "string"
-            # )
-
-            # lyrics_features = vectorize_layer(lyrics_input)
-            # lyrics_features = keras.layers.Embedding(size_vocabulary, 7, name = "embedding")(lyrics_features)
-            # lyrics_features = keras.layers.Dropout(0.2)(lyrics_features)
-            # lyrics_features = keras.layers.GlobalAveragePooling1D()(lyrics_features)
-            # lyrics_features = keras.layers.Dropout(0.2)(lyrics_features)
-            # lyrics_features = keras.layers.Dense(64, activation='relu')(lyrics_features)
-
-            # scalar_features = keras.layers.Dense(64, activation='relu')(scalars_input)
-            # main = keras.layers.concatenate([lyrics_features, scalar_features], axis = 1)
-            # main = keras.layers.Dense(256, activation='relu')(main)
-            # main = keras.layers.Dense(64, activation='relu')(main)
-            # main = keras.layers.Dense(32, activation='relu')(main)
-            # output = keras.layers.Dense(num_genres, name = "genre", activation = 'softmax')(main)
-
-            # model = keras.Model(
-            #     inputs = [lyrics_input, scalars_input],
-            #     outputs = output)
-
-            # model.compile(optimizer = "adam",
-            #     loss = losses.SparseCategoricalCrossentropy(from_logits=False),
-            #     metrics=['accuracy'])
-
-
-   
-            model.load_weights("mnist-model/fake_model")
-
-            prediction = model.predict(lyrics)[0]
-
-            return render_template("submit.html", prediction = prediction)
-        except:
+            return render_template("submit.html", prediction = True)
+        except Exception as e:
+            print(e)
             return render_template("submit.html", error = True)
+#def result():
 
-# @app.route('/prediction/<filename>') 
-
-# def prediction(filename):
-#     #Step 1
-    
-#     with graph.as_default():
-#       set_session(sess)
-#       probabilities = model.predict(lyrics)[0,:]
-#       print(probabilities)
-#       #Step 4
-#       number_to_class = ['blues', 'country', 'hip hop', 'jazz', 'pop', 'reggae', 'rock']
-#       index = np.argsort(probabilities)
-#       predictions = {
-#         "class1":number_to_class[index[6]],
-#         "class2":number_to_class[index[5]],
-#         "class3":number_to_class[index[4]],
-#         "prob1":probabilities[index[6]],
-#         "prob2":probabilities[index[5]],
-#         "prob3":probabilities[index[4]],
-#       }
-#     #Step 5
-#     return render_template('predict.html', predictions=predictions)
-
-# app.run(host='0.0.0.0', port=80)
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     #For rendering results on HTML GUI
-#     int_features = [float(x) for x in request.form.values()]
-#     final_features = [np.array(int_features)]
-#     prediction = model.predict(final_features)
-#     output = round(prediction[0], 2) 
-#     return render_template('index.html', prediction_text='The Predicted result is :{}'.format(output))
