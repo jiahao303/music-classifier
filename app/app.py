bee39b17acd31f96e2a3d9dd45e60e6a8e6b46ae
Tiana0117 <yining0117@g.ucla.edu>
2021-12-03 00:19:42 -0800

    css uploaded

--- app/app.py
+++ app/app.py
@@ -125,6 +125,7 @@ def submit():
             
             # output = get_track_features("7qiZfU4dY1lWllzX7mPBI3?si=058bcde867c24a5e")
             # lyrics = "The club isn't the best place to find a lover So the bar is where I go Me and my friends at the table doing shots Drinking fast and then we talk slow And you come over and start up a conversation with just me And trust me I'll give it a chance now Take my hand, stop, put Van the Man on the jukebox And then we start to dance, and now I'm singing like Girl, you know I want your love Your love was handmade for somebody like me Come on now, follow my lead I may be crazy, don't mind me Say, boy, let's not talk too much Grab on my waist and put that body on me Come on now, follow my lead Come, come on now, follow my lead I'm in love with the shape of you We push and pull like a magnet do Although my heart is falling too I'm in love with your body And last night you were in my room And now my bedsheets smell like you Every day discovering something brand new I'm in love with your body Oh-I-oh-I-oh-I-oh-I I'm in love with your body Oh-I-oh-I-oh-I-oh-I I'm in love with your body Oh-I-oh-I-oh-I-oh-I I'm in love with y"
+            # First standardize the lyrics
             dictionary = {'lyrics':[lyrics]}
             lyrics = lyrics.lower()
             #lyrics = lyrics.translate(str.maketrans("", "", string.punctuation))
@@ -132,9 +133,7 @@ def submit():
             stop_words = set(stopwords.words("english"))
             lyrics = lyrics.split()
             lyrics = " ".join([w for w in lyrics if w not in stop_words])
-            print(lyrics)
             lyrics = pd.DataFrame(dictionary,index=[0])
-
 
             lyrics = tf.constant(lyrics)
             lyrics = lyrics[None,:]
